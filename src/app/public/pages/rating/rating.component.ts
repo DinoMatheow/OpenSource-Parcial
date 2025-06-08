c97490ed149809a5600cffb557ea2518561e85fc
Matheow <95660245+DinoMatheow@users.noreply.github.com>
2025-06-08 00:32:25 -0500

    desing of form

--- src/app/public/pages/rating/rating.component.ts
+++ src/app/public/pages/rating/rating.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -8,4 +9,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './rating.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RatingComponent { }
+export class RatingComponent {
+
+private fb = inject(FormBuilder);
+formUtils = FormBuilder;
+
+myForm: FormGroup = this.fb.group({
+  name: ['', Validators.required],
+  ticket: ['', [Validators.required, Validators.email]]
+
+});
+
+}
